ebfe94f9539c878357ec673186816858e8dd9717
Dheeraj <dheeeraj0259@gmail.com>
2020-02-23 16:53:27 -0600

    added UI for login page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,13 +1,30 @@
 import React from "react";
-
+import { Grid, Fab } from "@material-ui/core";
+import LockIcon from "@material-ui/icons/Lock";
 import LoginCard from "../components/LoginCard";
 
 class Login extends React.Component {
   render() {
     return (
-      <div>
+      <Grid
+        container
+        direction="row"
+        justify="center"
+        style={{ marginTop: 150 }}
+      >
+        <Fab
+          size="medium"
+          aria-label="add"
+          style={{
+            position: "absolute",
+            marginTop: 8,
+            marginLeft: -10
+          }}
+        >
+          <LockIcon />
+        </Fab>
         <LoginCard />
-      </div>
+      </Grid>
     );
   }
 }
--- src/components/LoginCard.jsx
+++ src/components/LoginCard.jsx
@@ -1,42 +1,109 @@
 import React from "react";
 import {
   Card,
-  CardActionArea,
-  CardActions,
-  CardContent,
   CardMedia,
-  Button,
-  Typography
+  Typography,
+  Grid,
+  TextField,
+  InputAdornment,
+  IconButton,
+  OutlinedInput,
+  FormControl,
+  InputLabel,
+  Button
 } from "@material-ui/core";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+
 import loginBackground from "../images/loginBackground.jpg";
 
 export default function LoginCard() {
+  const [values, setValues] = React.useState({
+    userEmail: "",
+    password: "",
+    showPassword: false
+  });
+
+  const handleChange = key => event => {
+    setValues({ ...values, [key]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+
+  const handleClickSignIn = () => {
+    console.log(">>>>>>", values);
+  };
+
   return (
-    <Card style={{ maxWidth: 700 }}>
-      <CardActionArea>
-        <CardMedia
-          style={{ height: 300 }}
-          image={loginBackground}
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
+    <Card style={{ maxWidth: 1000 }}>
+      <Grid container direction="row" justify="center" spacing={3}>
+        <Grid item xs={6}>
+          <CardMedia
+            component="img"
+            style={{ height: 480 }}
+            image={loginBackground}
+            title="Login"
+          />
+        </Grid>
+
+        <Grid item xs={6}>
+          <Grid style={{ marginLeft: 10, marginTop: 50 }}>
+            <Typography gutterBottom variant="h4">
+              Sign in
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              Enter details to sign in
+            </Typography>
+          </Grid>
+
+          <Grid
+            container
+            direction="column"
+            style={{ marginTop: 20, padding: 20 }}
+          >
+            <TextField
+              id="outlined-required"
+              label="Email address"
+              onChange={handleChange("userEmail")}
+              variant="outlined"
+            />
+            <FormControl style={{ marginTop: 10 }} variant="outlined">
+              <InputLabel htmlFor="outlined-adornment-password">
+                Password
+              </InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                label="Password"
+                type={values.showPassword ? "text" : "password"}
+                value={values.password}
+                onChange={handleChange("password")}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      edge="end"
+                    >
+                      {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+            <Button
+              size="large"
+              style={{ marginTop: 40 }}
+              variant="contained"
+              color="primary"
+              onClick={handleClickSignIn}
+            >
+              Sign in
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
     </Card>
   );
 }
