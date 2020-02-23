fbcf8c2788e4a42fd97f3cfd964231d9fae30d67
Dheeraj <dheeeraj0259@gmail.com>
2020-02-23 13:34:42 -0600

    Initializing the login page design

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { getDataRequest } from "./actions/data";
+import "./App.css";
 
 class App extends Component {
   componentDidMount() {
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,22 +1,12 @@
 import React from "react";
-import { Button } from "@material-ui/core";
 
-function onClickLoginHandler() {
-  console.log("Clicked on login button");
-}
+import LoginCard from "../components/LoginCard";
 
 class Login extends React.Component {
   render() {
     return (
       <div>
-        <h1>Welocome to login page</h1>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => onClickLoginHandler()}
-        >
-          Login
-        </Button>
+        <LoginCard />
       </div>
     );
   }
