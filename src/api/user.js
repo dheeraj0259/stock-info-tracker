177dba1a689a4e00386eff41d22568a61ef42814
Dheeraj <dheeeraj0259@gmail.com>
2020-03-04 11:46:53 -0600

    - moved api base url to a constant file - minor refactor

--- src/api/user.js
+++ src/api/user.js
@@ -3,21 +3,17 @@ import axios from "axios";
 import { getUsersRequest } from "../actions/user";
 import { setAlertStatus } from "../actions/alert";
 import { setLoadingStatus } from "../actions/loading";
+import { baseApiUrl } from "../constants";
 
 export const getUserList = () => {
-  return axios.get(
-    "https://g6crplts3e.execute-api.us-east-2.amazonaws.com/local/userInfo"
-  );
+  return axios.get(`${baseApiUrl}/userInfo`);
 };
 
 export const setUserInformation = (userDetails, dispatch) => {
   try {
     dispatch(setLoadingStatus(true));
     axios
-      .post(
-        "https://g6crplts3e.execute-api.us-east-2.amazonaws.com/local/userInfo",
-        userDetails
-      )
+      .post(`${baseApiUrl}/userInfo`, userDetails)
       .then(res => {
         dispatch(setLoadingStatus(false));
         dispatch(
@@ -45,39 +41,29 @@ export const logIn = (userDetails, dispatch) => {
   try {
     const { email, password } = userDetails;
     dispatch(setLoadingStatus(true));
-    axios
-      .get(
-        `https://g6crplts3e.execute-api.us-east-2.amazonaws.com/local/userInfo/${email}`
-      )
-      .then(res => {
-        dispatch(setLoadingStatus(false));
-        if (!res.data.data.Item) {
-          dispatch(
-            setAlertStatus({
-              status: true,
-              message: res.data.message,
-              type: "warning"
-            })
-          );
-        } else if (res.data.data.Item.password !== password) {
-          dispatch(
-            setAlertStatus({
-              status: true,
-              message: `Incorrect password for the account ${email}`,
-              type: "warning"
-            })
-          );
-        } else {
-          dispatch(
-            setAlertStatus({
-              status: true,
-              message: res.data.message,
-              type: "success"
-            })
-          );
-          setTimeout(() => dispatch(setAlertStatus({})), 5000);
-        }
-      });
+
+    axios.get(`${baseApiUrl}/userInfo/${email}`).then(res => {
+      let { message } = res.data;
+      let type = "success";
+
+      if (!res.data.data.Item) {
+        type = "warning";
+      } else if (res.data.data.Item.password !== password) {
+        type = "warning";
+        message = `Incorrect password for the account ${email}`;
+      }
+
+      dispatch(setLoadingStatus(false));
+      dispatch(
+        setAlertStatus({
+          status: true,
+          message,
+          type
+        })
+      );
+      if (type === "success")
+        setTimeout(() => dispatch(setAlertStatus({})), 5000);
+    });
   } catch (e) {
     dispatch(
       setAlertStatus({
---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+const baseApiUrl =
+  "https://g6crplts3e.execute-api.us-east-2.amazonaws.com/local";
+
+module.exports = {
+  baseApiUrl
+};
