a5da842b15844fb9cfeb8201f0ee66e9dd167fdc
Dheeraj <dheeeraj0259@gmail.com>
2020-03-08 10:03:12 -0500

    - added animation to the dashboard cards

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,7 +7,7 @@ import {
   ExpansionPanelDetails,
   Typography,
   Button,
-  Zoom,
+  Grow,
   Icon
 } from "@material-ui/core";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
@@ -22,35 +22,41 @@ class Home extends Component {
     return (
       <Grid container style={{ marginTop: 50 }}>
         <Grid item md={12} style={{ padding: "0 20px 0 20px" }}>
-          <ExpansionPanel defaultExpanded>
-            <ExpansionPanelSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel1a-content"
-              id="panel1a-header"
+          <Grow in timeout={500}>
+            <ExpansionPanel
+              defaultExpanded
+              style={{ backgroundColor: "#80808026" }}
             >
-              <Typography>Data Visualization</Typography>
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails>
-              <Typography>Place holder for the graphs</Typography>
-            </ExpansionPanelDetails>
-          </ExpansionPanel>
-          <ExpansionPanel defaultExpanded>
-            <ExpansionPanelSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel2a-content"
-              id="panel2a-header"
+              <ExpansionPanelSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography variant="h5" style={{ color: "darkgrey" }}>
+                  Data Visualization
+                </Typography>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails>
+                <Typography>Place holder for the graphs</Typography>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+          </Grow>
+          <Grow in style={{ transformOrigin: "0 0 0" }} timeout={1000}>
+            <ExpansionPanel
+              defaultExpanded
+              style={{ backgroundColor: "#80808026" }}
             >
-              <Typography>Stock Information</Typography>
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails>
-              <Typography>
-                Actions:
-                <Zoom
-                  in
-                  style={{
-                    transitionDelay: "200ms"
-                  }}
-                >
+              <ExpansionPanelSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel2a-content"
+                id="panel2a-header"
+              >
+                <Typography variant="h5" style={{ color: "darkgrey" }}>
+                  Stock Information
+                </Typography>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails>
+                <Grid container justify="flex-end">
                   <Button
                     variant="contained"
                     size="small"
@@ -60,19 +66,19 @@ class Home extends Component {
                   >
                     ADD
                   </Button>
-                </Zoom>
-                <Button
-                  variant="contained"
-                  size="small"
-                  color="secondary"
-                  endIcon={<Icon>delete</Icon>}
-                  style={{ marginLeft: 5 }}
-                >
-                  DELETE
-                </Button>
-              </Typography>
-            </ExpansionPanelDetails>
-          </ExpansionPanel>
+                  <Button
+                    variant="contained"
+                    size="small"
+                    color="secondary"
+                    endIcon={<Icon>delete</Icon>}
+                    style={{ marginLeft: 5 }}
+                  >
+                    DELETE
+                  </Button>
+                </Grid>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+          </Grow>
         </Grid>
       </Grid>
     );
