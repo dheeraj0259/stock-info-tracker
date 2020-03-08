9234852fb0d5286e5d515ebdb59e5a5acdadc9b3
Dheeraj <dheeeraj0259@gmail.com>
2020-03-07 18:58:41 -0600

    - minor style change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { AppBar, Toolbar, IconButton, Typography } from "@material-ui/core";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Grid
+} from "@material-ui/core";
 import HomeIcon from "@material-ui/icons/Home";
 
 export default function Navbar() {
   return (
-    <AppBar style={{ background: "black" }}>
+    <AppBar style={{ background: "#111116" }}>
       <Toolbar>
-        <Link
-          to="/stock-info-tracker/"
-          style={{ color: "white", textDecoration: "none" }}
-        >
-          <IconButton edge="start" color="inherit" aria-label="menu">
-            <HomeIcon style={{ marginRight: "10px" }} />
-            <Typography variant="h6">Stock Info Tracker</Typography>
-          </IconButton>
-        </Link>
+        <Grid container direction="row" alignItems="center">
+          <Link
+            to="/stock-info-tracker/"
+            style={{ color: "white", textDecoration: "none" }}
+          >
+            <IconButton edge="start" color="inherit" aria-label="menu">
+              <HomeIcon style={{ marginRight: "2px" }} />
+              <Typography variant="h6">Stock Info Tracker</Typography>
+            </IconButton>
+          </Link>
+        </Grid>
       </Toolbar>
     </AppBar>
   );
