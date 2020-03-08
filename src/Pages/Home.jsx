d5475a3be3a22369d6adea45b9d5061ba3460c03
Dheeraj <dheeeraj0259@gmail.com>
2020-03-07 19:37:56 -0600

    - started on dashboard

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,15 +1,81 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Grid } from "@material-ui/core";
+import {
+  Grid,
+  ExpansionPanel,
+  ExpansionPanelSummary,
+  ExpansionPanelDetails,
+  Typography,
+  Button,
+  Zoom,
+  Icon
+} from "@material-ui/core";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 class Home extends Component {
   componentDidMount() {
     const { history, userAccess } = this.props;
-    if (!userAccess) history.push("/stock-info-tracker/signin");
+    //  if (!userAccess) history.push("/stock-info-tracker/signin");
   }
 
   render() {
-    return <h1>Welcome to dashboard</h1>;
+    return (
+      <Grid container style={{ marginTop: 50 }}>
+        <Grid item md={12} style={{ padding: "0 20px 0 20px" }}>
+          <ExpansionPanel defaultExpanded>
+            <ExpansionPanelSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography>Data Visualization</Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <Typography>Place holder for the graphs</Typography>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+          <ExpansionPanel defaultExpanded>
+            <ExpansionPanelSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel2a-content"
+              id="panel2a-header"
+            >
+              <Typography>Stock Information</Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <Typography>
+                Actions:
+                <Zoom
+                  in
+                  style={{
+                    transitionDelay: "200ms"
+                  }}
+                >
+                  <Button
+                    variant="contained"
+                    size="small"
+                    color="primary"
+                    endIcon={<Icon>add</Icon>}
+                    style={{ marginLeft: 5 }}
+                  >
+                    ADD
+                  </Button>
+                </Zoom>
+                <Button
+                  variant="contained"
+                  size="small"
+                  color="secondary"
+                  endIcon={<Icon>delete</Icon>}
+                  style={{ marginLeft: 5 }}
+                >
+                  DELETE
+                </Button>
+              </Typography>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+        </Grid>
+      </Grid>
+    );
   }
 }
 
