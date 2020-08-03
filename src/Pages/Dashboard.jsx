95687409929e47fcc559c718d012ae7c29d3310a
Dheeraj Yarramsetty <dheerajyarramsetty@Dheerajs-MacBook-Pro.local>
2020-08-02 21:57:24 -0500

    added redirect message when no user access found

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -25,94 +25,107 @@ class Dashboard extends Component {
 
   componentDidMount() {
     const { history, userAccess } = this.props;
-    //  if (!userAccess) history.push("/stock-info-tracker/signin");
+     if (!userAccess) setTimeout(() => history.push("/stock-info-tracker/signin"), 5000);
   }
 
   render() {
+    const { userAccess } = this.props;
     const { showFilter } = this.state;
-    return (
-      <Grid container direction="row" style={{ marginTop: 50 }}>
-        <FilterDrawer
-          showFilter={showFilter}
-          closeDrawer={() => this.setState({ showFilter: false })}
-        />
-        <Grid item md={12} style={{ padding: "0 20px 0 20px" }}>
-          <Grow in style={{ transformOrigin: "0 0 0" }} timeout={500}>
-            <ExpansionPanel
-              defaultExpanded
-              style={{ backgroundColor: "transparent", boxShadow: "none" }}
-            >
-              <ExpansionPanelSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
+    const renderDashboardContent = () => {
+      return (
+        <Grid container direction="row" style={{ marginTop: 50 }}>
+          <FilterDrawer
+            showFilter={showFilter}
+            closeDrawer={() => this.setState({ showFilter: false })}
+          />
+          <Grid item md={12} style={{ padding: "0 20px 0 20px" }}>
+            <Grow in style={{ transformOrigin: "0 0 0" }} timeout={500}>
+              <ExpansionPanel
+                defaultExpanded
+                style={{ backgroundColor: "transparent", boxShadow: "none" }}
               >
-                <Typography variant="h5" style={{ color: "darkgrey" }}>
-                  Data Visualization
-                </Typography>
-              </ExpansionPanelSummary>
-              <ExpansionPanelDetails>
-                <Typography>Place holder for the graphs</Typography>
-              </ExpansionPanelDetails>
-            </ExpansionPanel>
-          </Grow>
-          <Divider variant="middle" />
-          <Grow in style={{ transformOrigin: "0 0 0" }} timeout={1000}>
-            <ExpansionPanel
-              defaultExpanded
-              style={{ backgroundColor: "transparent", boxShadow: "none" }}
-            >
-              <ExpansionPanelSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel2a-content"
-                id="panel2a-header"
+                <ExpansionPanelSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <Typography variant="h5" style={{ color: "darkgrey" }}>
+                    Data Visualization
+                  </Typography>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
+                  <Typography>Place holder for the graphs</Typography>
+                </ExpansionPanelDetails>
+              </ExpansionPanel>
+            </Grow>
+            <Divider variant="middle" />
+            <Grow in style={{ transformOrigin: "0 0 0" }} timeout={1000}>
+              <ExpansionPanel
+                defaultExpanded
+                style={{ backgroundColor: "transparent", boxShadow: "none" }}
               >
-                <Typography variant="h5" style={{ color: "darkgrey" }}>
-                  Stock Information
-                </Typography>
-              </ExpansionPanelSummary>
-              <ExpansionPanelDetails>
-                <Grid container justify="flex-end">
-                  <Grid item>
-                    <Button
-                      variant="contained"
-                      size="small"
-                      color="primary"
-                      endIcon={<Icon>add</Icon>}
-                      style={{ marginLeft: 5 }}
-                    >
-                      ADD
-                    </Button>
-                    <Button
-                      variant="contained"
-                      size="small"
-                      color="secondary"
-                      endIcon={<Icon>delete</Icon>}
-                      style={{ marginLeft: 5 }}
-                    >
-                      DELETE
-                    </Button>
-                    <Button
-                      variant="contained"
-                      size="small"
-                      color="inherit"
-                      endIcon={<FilterListIcon />}
-                      style={{ marginLeft: 5 }}
-                      onClick={() => this.setState({ showFilter: true })}
-                    >
-                      Filter
-                    </Button>
+                <ExpansionPanelSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel2a-content"
+                  id="panel2a-header"
+                >
+                  <Typography variant="h5" style={{ color: "darkgrey" }}>
+                    Stock Information
+                  </Typography>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
+                  <Grid container justify="flex-end">
+                    <Grid item>
+                      <Button
+                        variant="contained"
+                        size="small"
+                        color="primary"
+                        endIcon={<Icon>add</Icon>}
+                        style={{ marginLeft: 5 }}
+                      >
+                        ADD
+                      </Button>
+                      <Button
+                        variant="contained"
+                        size="small"
+                        color="secondary"
+                        endIcon={<Icon>delete</Icon>}
+                        style={{ marginLeft: 5 }}
+                      >
+                        DELETE
+                      </Button>
+                      <Button
+                        variant="contained"
+                        size="small"
+                        color="inherit"
+                        endIcon={<FilterListIcon />}
+                        style={{ marginLeft: 5 }}
+                        onClick={() => this.setState({ showFilter: true })}
+                      >
+                        Filter
+                      </Button>
+                    </Grid>
+                    <Grid item xs={12} style={{ marginTop: 10 }}>
+                      <Stocktable />
+                    </Grid>
                   </Grid>
-                  <Grid item xs={12} style={{ marginTop: 10 }}>
-                    <Stocktable />
-                  </Grid>
-                </Grid>
-              </ExpansionPanelDetails>
-            </ExpansionPanel>
-          </Grow>
+                </ExpansionPanelDetails>
+              </ExpansionPanel>
+            </Grow>
+          </Grid>
         </Grid>
-      </Grid>
-    );
+      );
+    }
+
+    const NoAccessMessage = () => {
+      return (
+        <div style={{ marginTop: 50 }}>
+          <h1>No User Access redirecting in 5 sec</h1>
+        </div>
+      )
+    }
+
+    return userAccess ? renderDashboardContent() : NoAccessMessage();
   }
 }
 
